c67fa8d38da40fc94558c6fced40d64c3e0fd87e
Isac <91141474+Isac-AS@users.noreply.github.com>
2024-02-04 16:56:54 +0000

    Partially Works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use once_cell::sync::Lazy;
 use log::LevelFilter;
-mod models;
+use once_cell::sync::Lazy;
 mod controllers;
+mod models;
 mod pdf_parser;
 
-use controllers::questionnaire_controller::create::{ upload_pdf, create_questionnaire };
-use controllers::questionnaire_controller::delete::delete_questionnaire;
-use controllers::questionnaire_controller::read::{ get_questionnaire, get_questionnaires };
-use controllers::questionnaire_controller::update::{ update_questionnaire_name, touch_questionnaire };
 use controllers::question_controller::delete::delete_question;
 use controllers::question_controller::read::get_questions;
 use controllers::question_controller::update::update_question;
+use controllers::questionnaire_controller::create::{create_questionnaire, upload_pdf};
+use controllers::questionnaire_controller::delete::delete_questionnaire;
+use controllers::questionnaire_controller::read::{get_questionnaire, get_questionnaires};
+use controllers::questionnaire_controller::update::{
+    touch_questionnaire, update_questionnaire_name,
+};
 use tauri::Manager;
 
 static DATABASE_BUILDER: Lazy<native_db::DatabaseBuilder> = Lazy::new(|| {
@@ -29,8 +31,8 @@ static DATABASE_BUILDER: Lazy<native_db::DatabaseBuilder> = Lazy::new(|| {
 
 fn main() {
     let _ = simple_logging::log_to_file("cuestionektor.log", LevelFilter::Trace);
-    
-    let db = DATABASE_BUILDER.create("./cuestionektorDB");
+
+    let db = DATABASE_BUILDER.create("./cuestionektor_DB").expect("failed to create database");
 
     tauri::Builder::default()
         .setup(move |app| {
